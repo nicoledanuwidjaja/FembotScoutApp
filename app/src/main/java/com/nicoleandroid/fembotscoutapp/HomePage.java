1ee91c794709a73cb0f3f8202bd37c4c56893cd9
Nicole Danuwidjaja <danuwidjajanicole@gmail.com>
2016-07-08 00:18:13 -0700

    v1.0

---
+++ app/src/main/java/com/nicoleandroid/fembotscoutapp/HomePage.java
@@ -0,0 +1,49 @@
+package com.nicoleandroid.fembotscoutapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        Button HomePage = (Button) findViewById(R.id.New_Profile);
+        HomePage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent =
+                        new Intent(view.getContext(), NewTeamProfile.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home_page, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
